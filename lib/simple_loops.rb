033bf6e3905d363d4166c340a508e3b1e01da539
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-07-09 17:47:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,6 +14,10 @@ end
 
 def return_string_array(array)
   count = 0
-    array << array.push[count].to_s
-    
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1 
+  end
+  new_array
 end
